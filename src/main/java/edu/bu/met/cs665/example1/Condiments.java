222c4bfee2a289c40b3d9fbf541e19160116e4e5
Shohaib Mallick <shohaibm99@gmail.com>
2024-04-25 13:59:43 -0400

    Final

--- src/main/java/edu/bu/met/cs665/example1/Condiments.java
+++ src/main/java/edu/bu/met/cs665/example1/Condiments.java
@@ -2,52 +2,62 @@ package edu.bu.met.cs665.example1;
 
 import java.util.Scanner;
 
+
 public class Condiments {
+    private static final int MIN_UNITS = 0;
+    private static final int MAX_UNITS = 3;
+    private final int[] units;
 
-    private int[] units = new int[2];
+    public Condiments() {
+        units = new int[2];
+    }
 
+    /**
+     * method to add condiments to drinks
+     * @return int
+     */
     public int[] addCondiments() {
-        int temp;
-        boolean invalid = true;
         Scanner scanner = new Scanner(System.in);
-
-        while (invalid) {
-            System.out.print("Number of units of Milk (0-3): ");
-            temp = scanner.nextInt();
-            if (temp > 3 || temp < 0) {
-                System.out.println("Invalid number of units !!!");
-            } else {
-                units[0] = temp;
-                invalid = false;
-            }
-        }
-
-        invalid = true;
-        while (invalid) {
-            System.out.print("Number of units of Sugar (0-3): ");
-            temp = scanner.nextInt();
-            if (temp > 3 || temp < 0) {
-                System.out.println("Invalid number of units !!!");
-            } else {
-                units[1] = temp;
-                invalid = false;
-            }
-        }
-
+        setUnits(scanner, "Milk");
+        setUnits(scanner, "Sugar");
         return units;
     }
 
+    /**
+     * method to set units
+     * @param scanner
+     * @param condimentName
+     * return void
+     */
+    private void setUnits(Scanner scanner, String condimentName) {
+        boolean invalid = true;
+        int index = condimentName.equalsIgnoreCase("Milk") ? 0 : 1;
+
+        while (invalid) {
+            System.out.print("Number of units of " + condimentName + " (0-3): ");
+            int temp = scanner.nextInt();
+            if (temp < MIN_UNITS || temp > MAX_UNITS) {
+                System.out.println("Invalid number of units !!!");
+            } else {
+                units[index] = temp;
+                invalid = false;
+            }
+        }
+    }
+
+    //getter for milk units
     public int getMilkUnits() {
         return units[0];
     }
 
+    //getter for sugar units
     public int getSugarUnits() {
         return units[1];
     }
 
-    public void setUnits(int[] units){
+    //setter
+    public void setUnits(int[] units) {
         this.units[0] = units[0];
         this.units[1] = units[1];
     }
 }
-
